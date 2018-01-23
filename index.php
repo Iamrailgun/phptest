ae3d9c106879a6bf8b9afae8eb82ae1c2c88061d
Iamrailgun <pombi4512@yandex.ru>
2018-01-23 12:19:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Мой первый PHP-шаблон</title>
+</head>
+<body>
+	<div id="wrapper">
+		<?php include 'include/header.php'; ?>
+		<?php include 'include/nav.php'; ?>
+		<?php include 'include/content.php'; ?>
+		<?php include 'include/sidebar.php'; ?>
+		<?php include 'include/footer.php'; ?>
+	</div>
+</body>
+</html>
